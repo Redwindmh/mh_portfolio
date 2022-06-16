cbc9aee099ef0a1e9af11cf4b85d565673bae6d5
Malcolm Hendricks <redwindmh@gmail.com>
2022-06-16 17:11:08 +0900

    Started work on sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Sidebar from '../Sidebar';
 import './index.css';
 
 const Layout = ()=> {
-    return <>Hello</>
+    return <><Sidebar /> I won't deny it, I'm straight ridah</>
 }
 
 export default Layout;
