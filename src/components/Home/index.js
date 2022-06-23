68360dca3dd0bf269470748e1ceaa3fb60bf8277
Malcolm <redwindmh@gmail.com>
2022-06-23 20:45:48 +0900

    More work on Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,8 +5,8 @@ import './index.css';
 const Home = () => {
 
     return (
-        <div className="container">
-            <div>
+        <div className="container home-page">
+            <div className="text-zone">
                 <h1>Hi, <br /> I'm Malcolm<br />web developer</h1>
                 <h2>Fullstack Developer / Unity & VR Developer</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
