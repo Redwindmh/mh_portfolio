382fe7a97ed2a2f462058c8b11db0b097c6d8e74
Malcolm <redwindmh@gmail.com>
2022-07-06 16:19:50 +0900

    Finished main struture of About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import AnimatedCharacters from '../AnimatedCharacters'
 
 const About = ()=> {
+
+    const [characterClass, setCharacterClass] = useState('text-animate');
+
+    useEffect (() => {
+        setTimeout(() => {
+            setCharacterClass('text-animate-hover')
+        }, 3000)
+    }, []);
+
  return (
     <div className="container about-page">
         <div className="text-zone">
             <h1>
-            <AnimatedCharacters strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15}/>
+            <AnimatedCharacters characterClass={characterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15}/>
             </h1>
             <p>
                 I am the creator, the great giving and taker
+            </p>
+            <p>
+                This is just a test
+            </p>
+            <p>
+                Puerto Ricans known for slashing
             </p>
         </div>
     </div>
