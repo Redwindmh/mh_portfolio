b6c7feb3d5c41536cd9bf97863f59fb8c7716fed
Malcolm Hendricks <redwindmh@gmail.com>
2022-07-22 18:27:10 +0900

    Another test

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
-      <Route exact path="/" component={Index} />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
